b0ff6188312e2c8258bbc550009609903ddb3bb5
MarVinCan <marcus.v.e.candido@gmail.com>
2022-04-06 15:39:31 -0300

    DepartmentFormController

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,56 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable {
+	
+	@FXML
+	private TextField txtdId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	@FXML
+	private Label labelErrorId;
+	
+	@FXML
+	private Label labelErroName;
+	
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("onBtSaveAction");
+	}
+	
+	
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNode();
+	}
+	
+	private void initializeNode() {
+		Constraints.setTextFieldInteger(txtdId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	
+
+}
