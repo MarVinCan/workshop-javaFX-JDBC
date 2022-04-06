9d90a9568bfe305c3e297fab2f96e1e17401f3c1
MarVinCan <marcus.v.e.candido@gmail.com>
2022-04-06 16:31:45 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -4,8 +4,19 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class Utils {
+	
 		
 	public static Stage currentStage(javafx.event.ActionEvent event) {
 		return (Stage)((Node)event.getSource()).getScene().getWindow();
+	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+		
+			return Integer.parseInt(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
 	}
 }
